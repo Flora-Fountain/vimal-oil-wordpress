b805db6f61caf13ef5a2b9f6d564b63ea0653b55
vimal5460 <vimalp.flora@gmail.com>
2023-05-26 09:54:47 +0530

    vimal

--- wp-content/themes/vimal-oil/template/privacy-policy.php
+++ wp-content/themes/vimal-oil/template/privacy-policy.php
@@ -9,7 +9,7 @@ echo get_header();
     <!--module-2 hero banner html code  -->
     <div class="privacy-banner default-section blue-bg-banner">
         <div class="container">
-            <div class="row">
+            <div class="row align-items-center">
                 <div class="col-lg-6 col-md-6 col-sm-12">
                     <div class="content-part">
                         <div class="h1-title">
@@ -19,14 +19,69 @@ echo get_header();
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-12">
                     <div class="image-part">
-                        
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/privacy-policy.webp" alt="privacy policy kiara image">
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
     
+    <div class="privacy-policy text-center default-section">
+        <div class="container">
+            <div class="h2-white">
+                <h2 class="text-noeffect">
+                    Privacy policy
+                </h2>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="privacy-policy text-center default-section">
+        <div class="container">
+            <div class="h2-white">
+                <h2 class="text-noeffect">
+                    Privacy assurance
+                </h2>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="privacy-policy text-center default-section footer-before">
+        <div class="container">
+            <div class="h2-white">
+                <h2 class="text-noeffect">
+                    Grievance redressal
+                </h2>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+        </div>
+    </div>
 
     
 <?php 
--- wp-content/themes/vimal-oil/template/terms-and-conditions.php
+++ wp-content/themes/vimal-oil/template/terms-and-conditions.php
@@ -7,23 +7,80 @@ echo get_header();
 ?>
    
    <!--module-2 hero banner html code  -->
-   <div class="hero-banner default-section">
-        banner
+   <div class="privacy-banner default-section blue-bg-banner">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <div class="content-part">
+                        <div class="h1-title">
+                            Understanding our privacy policy
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <div class="image-part">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/privacy-policy.webp" alt="privacy policy kiara image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="privacy-policy text-center default-section">
+        <div class="container">
+            <div class="h2-white">
+                <h2 class="text-noeffect">
+                    Disclaimer
+                </h2>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+        </div>
     </div>
 
-    <!-- module-3 Terms and services -->
-    <div class="health-section default-section">
-        Terms and services
+    <div class="privacy-policy text-center default-section">
+        <div class="container">
+            <div class="h2-white">
+                <h2 class="text-noeffect">
+                    Caution notice
+                </h2>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+        </div>
     </div>
-
-    <!-- module-4 payment policy  -->
-    <div class="product-section default-section">
-        payment policy
-    </div>
-
-    <!-- module-5 use abligation  -->
-    <div class="default-section nrt-ills">
-        useR abligation
+    
+    <div class="privacy-policy text-center default-section footer-before">
+        <div class="container">
+            <div class="h2-white">
+                <h2 class="text-noeffect">
+                    User obligations
+                </h2>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+        </div>
     </div>
    
 <?php 
