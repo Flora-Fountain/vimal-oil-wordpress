50b39bb08f20939e4dbdf0f73a5593d5f167ecd2
vimal5460 <vimalp.flora@gmail.com>
2023-05-28 07:58:19 +0530

    vimal

--- wp-content/themes/vimal-oil/template/career-jd-page.php
+++ wp-content/themes/vimal-oil/template/career-jd-page.php
@@ -7,17 +7,31 @@ echo get_header();
 ?>
     
     <!--module-2 hero banner html code  -->
-    <div class="hero-banner default-section">
-        banner
+    <div class="common-banner default-section blue-bg-banner">
+        <div class="cb-content">
+            <div class="h1-title">
+                <h1 class="text-noeffect">Reach out to us!</h1>
+            </div>
+            <div class="banner-para  text-center">
+                <p class="text-noeffect">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                </p>
+            </div>
+            <div class="submit-button text-center">
+                <button class="btn-effect" type="submit">
+                    Know More
+                </button>
+            </div>
+        </div>
     </div>
 
     <!-- module-3 job description section -->
-    <div class="health-section default-section">
+    <div class="job-description default-section">
         job description
     </div>
 
     <!-- module-4 form section  -->
-    <div class="product-section default-section">
+    <div class="form-section default-section footer-before">
         form
     </div>
 <?php 
