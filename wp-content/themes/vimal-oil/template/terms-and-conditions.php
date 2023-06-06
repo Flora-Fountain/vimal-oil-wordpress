1250262c595fc4fd252341494169517df4b1eecd
PRIYANKA-FLORA <124249400+PRIYANKA-FLORA@users.noreply.github.com>
2023-06-06 18:26:09 +0530

    privacy policy

--- wp-content/themes/vimal-oil/template/terms-and-conditions.php
+++ wp-content/themes/vimal-oil/template/terms-and-conditions.php
@@ -7,7 +7,7 @@ echo get_header();
 ?>
    
    <!--module-2 hero banner html code  -->
-   <div class="privacy-banner default-section blue-bg-banner">
+    <div class="privacy-banner default-section blue-bg-banner">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-6 col-md-6 col-sm-12">
@@ -26,7 +26,7 @@ echo get_header();
         </div>
     </div>
     
-    <div class="privacy-policy text-center default-section">
+    <div class="default-section privacy-policy footer-before">
         <div class="container">
             <div class="h2-white">
                 <h2 class="text-noeffect">
@@ -35,18 +35,10 @@ echo get_header();
             </div>
             <div class="pp-para">
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            </div>
-            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
             </div>
-            <div class="pp-para">
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            </div>
-        </div>
-    </div>
 
-    <div class="privacy-policy text-center default-section">
-        <div class="container">
             <div class="h2-white">
                 <h2 class="text-noeffect">
                     Caution notice
@@ -54,18 +46,9 @@ echo get_header();
             </div>
             <div class="pp-para">
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            </div>
-            <div class="pp-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
             </div>
-            <div class="pp-para">
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            </div>
-        </div>
-    </div>
-    
-    <div class="privacy-policy text-center default-section footer-before">
-        <div class="container">
             <div class="h2-white">
                 <h2 class="text-noeffect">
                     User obligations
@@ -73,16 +56,12 @@ echo get_header();
             </div>
             <div class="pp-para">
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            </div>
-            <div class="pp-para">
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            </div>
-            <div class="pp-para">
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
             </div>
         </div>
     </div>
-   
+
 <?php 
     echo get_footer();
 ?>
