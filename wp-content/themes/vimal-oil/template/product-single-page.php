3aa9571f14e4068b31756c0479c7528f0f9178d3
vimal5460 <vimalp.flora@gmail.com>
2023-06-08 12:44:47 +0530

    vimal

--- wp-content/themes/vimal-oil/template/product-single-page.php
+++ wp-content/themes/vimal-oil/template/product-single-page.php
@@ -7,13 +7,107 @@ echo get_header();
 ?>
     
     <!--module-2 hero banner html code  -->
-    <div class="hero-banner default-section">
-        banner
+    <div class="product-sinle-banner default-section blue-bg-banner">
+            <div class="container">
+                <div class="banner-slider owl-carousel owl-theme">
+                    <div class="item">
+                        <div class="banner-info">
+                            <div class="row align-items-center">
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="banner-right">
+                                        <div class="h1-title">
+                                            <h1 class="text-noeffect">Khaane me lagao Sehat ka tadka</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="banner-left product-img">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/kachi-ghani-oil.png" alt="kachi ghani oil" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="banner-info">
+                            <div class="row align-items-center">
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="banner-right">
+                                        <div class="h1-title">
+                                            <h1 class="text-noeffect">Khaane me lagao Sehat ka tadka</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="banner-left product-img">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/kachi-ghani-oil.png" alt="kachi ghani oil" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="banner-info">
+                            <div class="row align-items-center">
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="banner-right">
+                                        <div class="h1-title">
+                                            <h1 class="text-noeffect">Khaane me lagao Sehat ka tadka</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="banner-left product-img">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/kachi-ghani-oil.png" alt="kachi ghani oil" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="banner-info">
+                            <div class="row align-items-center">
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="banner-right">
+                                        <div class="h1-title">
+                                            <h1 class="text-noeffect">Khaane me lagao Sehat ka tadka</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="banner-left product-img">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/kachi-ghani-oil.png" alt="kachi ghani oil" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
     </div>
 
     <!-- module-3 oil details -->
-    <div class="health-section default-section">
-        
+    <div class="oil-details default-section">
+        <div class="container">
+            <div class="row align-aligns-center justify-content-center">
+                <div class="col-lg-6 col-md-12 col-sm-12">
+                    <div class="h2-title">
+                        <h2 class="text-noeffect">
+                            Tikha Kachi Ghani Musturd Oil
+                        </h2>
+                    </div>
+                    <div class="oil-para">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita doloremque maxime sapiente quos provident ipsa quae rerum architecto molestiae corrupti explicabo amet non fugiat sequi et, nisi voluptate odit natus!</p>
+                    </div>
+                    <div class="oil-size">
+
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-12">
+
+                </div>
+            </div>
+        </div>
     </div>
 
     <!-- module-4 oil benefits section  -->
