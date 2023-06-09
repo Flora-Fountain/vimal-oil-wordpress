ae0feb08a0c48455a305e16f7324ad8a7caddd01
vimal5460 <vimalp.flora@gmail.com>
2023-06-09 18:48:55 +0530

    vimal

--- wp-content/themes/vimal-oil/template/oil-calendar.php
+++ wp-content/themes/vimal-oil/template/oil-calendar.php
@@ -31,14 +31,13 @@ echo get_header();
                     </div>
                 </div>
                 <div class="col-lg-6 colp-md-6 col-sm-12">
-                <div class="quiz-cel">
+                <div class="quiz-cel" id="con-run">
 						<div class="box-image text-center">
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/quiz-end-box.png" alt="quiz end banner image" />
                         </div>
                         <div class="oil-cans">
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/oil-cans.png" alt="quiz end banner image" />
                         </div>
-                        <div class="confety" id="confeti"></div>
 					</div>
                 </div>
             </div>
