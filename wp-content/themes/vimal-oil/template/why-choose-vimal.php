b0bbc1b0edb953d31b04c91eb675477dbf5a5bbd
vimal5460 <vimalp.flora@gmail.com>
2023-06-20 09:47:26 +0530

    vimal

--- wp-content/themes/vimal-oil/template/why-choose-vimal.php
+++ wp-content/themes/vimal-oil/template/why-choose-vimal.php
@@ -238,8 +238,6 @@ echo get_header();
                 </ul>
             </div>
             <div class="submit-button text-center">
-                <a href="<?php echo the_field('nrt_section_button_link');?>" class="btn-effect blue-back"><?php echo the_field('nrt_section_button_name');?></a>
-            <div class="submit-button text-center">
                 <a class="btn-effect blue-back" href="<?php echo the_field('nrt_section_button_link');?>" class="btn-effect"><?php echo the_field('nrt_section_button_name');?></a>
             </div>
         </div>
