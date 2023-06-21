e0148f81d17a25aae28c6c9bd1c64f50959d3d23
vimal5460 <vimalp.flora@gmail.com>
2023-06-21 18:32:58 +0530

    vimal

--- wp-content/themes/vimal-oil/template/home.php
+++ wp-content/themes/vimal-oil/template/home.php
@@ -531,19 +531,19 @@
                         <span class=""></span>
                         <div class="tinder--cards">
                             <div class="tinder--card" id="tinder--card">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cook-video.png" alt="social post" />
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cook-video-4.jpg" alt="social post" />
                             </div>
                             <div class="tinder--card" id="tinder--card">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cook-video.png" alt="social post" />
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cook-video-1.jpg" alt="social post" />
                             </div>
                             <div class="tinder--card" id="tinder--card">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cook-video.png" alt="social post" />
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cook-video-2.jpg" alt="social post" />
                             </div>
                             <div class="tinder--card" id="tinder--card">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cook-video.png" alt="social post" />
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cook-video-3.jpg" alt="social post" />
                             </div>
                             <div class="tinder--card" id="tinder--card">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cook-video.png" alt="social post" />
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cook-video-4.jpg" alt="social post" />
                             </div>
                             <span id="dp-prev">
                                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M15.7071 4.29289C16.0976 4.68342 16.0976 5.31658 15.7071 5.70711L9.41421 12L15.7071 18.2929C16.0976 18.6834 16.0976 19.3166 15.7071 19.7071C15.3166 20.0976 14.6834 20.0976 14.2929 19.7071L7.29289 12.7071C7.10536 12.5196 7 12.2652 7 12C7 11.7348 7.10536 11.4804 7.29289 11.2929L14.2929 4.29289C14.6834 3.90237 15.3166 3.90237 15.7071 4.29289Z"/></svg>
@@ -854,53 +854,53 @@
             nope.addEventListener("click", nopeListener);
             love.addEventListener("click", loveListener);
 
-            document.querySelector(".closeMe").addEventListener("click", function () {
-                toggleInfoClose(true);
-            });
-            document.querySelector(".covering").addEventListener("click", function () {
-                toggleInfoClose(true);
-            });
-            document.querySelector("#info").addEventListener("click", function () {
-                toggleInfoClose(false);
-            });
+            // document.querySelector(".closeMe").addEventListener("click", function () {
+            //     toggleInfoClose(true);
+            // });
+            // document.querySelector(".covering").addEventListener("click", function () {
+            //     toggleInfoClose(true);
+            // });
+            // document.querySelector("#info").addEventListener("click", function () {
+            //     toggleInfoClose(false);
+            // });
 
-            function toggleInfoClose(bool) {
-                if (!bool) {
-                    document.querySelector(".tinder--info").style.top = "50%";
-                } else {
-                    document.querySelector(".tinder--info").style.top = "200vh";
-                }
-            }
+            // function toggleInfoClose(bool) {
+            //     if (!bool) {
+            //         document.querySelector(".tinder--info").style.top = "50%";
+            //     } else {
+            //         document.querySelector(".tinder--info").style.top = "200vh";
+            //     }
+            // }
 
-            //stuff to prompt the user to swipe
-            //Shows the animation if your mouse is still for 15 seconds/hides it when you move
-            let fadein = null;
+            // //stuff to prompt the user to swipe
+            // //Shows the animation if your mouse is still for 15 seconds/hides it when you move
+            // let fadein = null;
 
-            const myFunction = (fadeOutTime, fadeInAfterTime) => {
-                document.querySelector(".promptBox").style.transition = fadeOutTime + "ms";
-                document.querySelector(".promptBox").style.opacity = "0";
-                if (fadein != null) {
-                    clearTimeout(fadein);
-                }
-                fadein = setTimeout(showMe, fadeInAfterTime);
-            };
+            // const myFunction = (fadeOutTime, fadeInAfterTime) => {
+            //     document.querySelector(".promptBox").style.transition = fadeOutTime + "ms";
+            //     document.querySelector(".promptBox").style.opacity = "0";
+            //     if (fadein != null) {
+            //         clearTimeout(fadein);
+            //     }
+            //     fadein = setTimeout(showMe, fadeInAfterTime);
+            // };
 
-            const showMe = () => {
-                document.querySelector(".promptBox").style.opacity = "1";
-            };
+            // const showMe = () => {
+            //     document.querySelector(".promptBox").style.opacity = "1";
+            // };
 
-            document.querySelector("body").addEventListener("mousemove", function () {
-                myFunction(300, 15000);
-            });
-            document.querySelector("body").addEventListener("click", function () {
-                myFunction(300, 15000);
-            });
-            document.querySelector("body").addEventListener("touchstart", function () {
-                myFunction(300, 15000);
-            });
-            document.querySelector("body").addEventListener("touchmove", function () {
-                myFunction(300, 15000);
-            });
+            // document.querySelector("body").addEventListener("mousemove", function () {
+            //     myFunction(300, 15000);
+            // });
+            // document.querySelector("body").addEventListener("click", function () {
+            //     myFunction(300, 15000);
+            // });
+            // document.querySelector("body").addEventListener("touchstart", function () {
+            //     myFunction(300, 15000);
+            // });
+            // document.querySelector("body").addEventListener("touchmove", function () {
+            //     myFunction(300, 15000);
+            // });
 
     </script>
 
